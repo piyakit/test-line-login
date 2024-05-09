0f30da117191342539f141ccea1aa0412ef52f73
piyakit <piyakit.tho@gmail.com>
2024-05-09 15:48:26 +0700

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,21 @@
-'use client'
+"use client";
 
-import Image from "next/image";
+import liff from "@line/liff";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
   useEffect(() => {
-    console.log(process.env.LINE_CLIENT_SECRET)
-  }, [])
+    liff.init({ liffId: "2004885407-o7z2m791" }).then(() => {
+      handleRedirect();
+    });
+  }, []);
+  const handleRedirect = () => {
+    const isLoggedIn = liff.isLoggedIn();
+    isLoggedIn ? router.push("/login") : router.push("/profile");
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
 }
